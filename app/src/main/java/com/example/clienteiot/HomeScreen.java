c3114d6bf4a142297b266f9f823f7a7acc0d2586
Enriquelap20 <17841136+Enriquelap20@users.noreply.github.com>
2019-12-29 13:44:28 -0400

    Cambio de cálculo de MD5 (Para concordar con LabView)

--- app/src/main/java/com/example/clienteiot/HomeScreen.java
+++ app/src/main/java/com/example/clienteiot/HomeScreen.java
@@ -246,23 +246,22 @@ public class HomeScreen extends AppCompatActivity {
         }
     }
 
-    public String md5(String s) {
+    private String md5(String in) {
+        MessageDigest digest;
         try {
-            // Create MD5 Hash
-            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
-            digest.update(s.getBytes());
-            byte[] messageDigest = digest.digest();
-
-            // Create Hex String
-            StringBuffer hexString = new StringBuffer();
-            for (int i=0; i<messageDigest.length; i++)
-                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
-
-            return hexString.toString();
-        }catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
+            digest = MessageDigest.getInstance("MD5");
+            digest.reset();
+            digest.update(in.getBytes());
+            byte[] a = digest.digest();
+            int len = a.length;
+            StringBuilder sb = new StringBuilder(len << 1);
+            for (int i = 0; i < len; i++) {
+                sb.append(Character.forDigit((a[i] & 0xf0) >> 4, 16));
+                sb.append(Character.forDigit(a[i] & 0x0f, 16));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
+        return null;
     }
 
 }
